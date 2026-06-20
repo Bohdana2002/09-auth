49210a5647830b6b447f5a4284e359a63f0e1042
Bohdana Turcheniak <bogdanaturcheniak@gmail.com>
2026-06-20 13:26:00 +0300

    fixed the mistake about metadata and divided the page into server and client

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,9 +1,5 @@
-
-import Image from "next/image";
-import Link from "next/link";
-import css from "./ProfilePage.module.css";
 import { Metadata } from "next";
-import { useAuthStore } from "@/lib/store/authStore";
+import ProfileClient from "./ProfileClient";
 
 export const metadata: Metadata = {
   title: "Profile page",
@@ -18,38 +14,13 @@ export const metadata: Metadata = {
         width: 1200,
         height: 630,
         alt: "Profile page",
-      }
-    ]
+      },
+    ],
   },
 };
 
 const ProfilePage = () => {
-  const user = useAuthStore((state) => state.user);
-  return (
-    <main className={css.mainContent}>
-      <div className={css.profileCard}>
-        <div className={css.header}>
-          <h1 className={css.formTitle}>Profile Page</h1>
-          <Link href="profile/edit" className={css.editProfileButton}>
-            Edit Profile
-          </Link>
-        </div>
-        <div className={css.avatarWrapper}>
-          <Image
-            src={user?.avatar || "/default.png"}
-            alt="User Avatar"
-            width={120}
-            height={120}
-            className={css.avatar}
-          />
-        </div>
-        <div className={css.profileInfo}>
-          <p>Username: {user?.username}</p>
-          <p>Email: {user?.email}</p>
-        </div>
-      </div>
-    </main>
-  );
+  return <ProfileClient />;
 };
 
 export default ProfilePage;
